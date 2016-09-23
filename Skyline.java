cf7879515cd57d8a63c068d46f061de6ff066609
Kenta Medina <kenta.medina@gmail.com>
2016-09-22 19:46:09 -0700

    replaced comments and added javadoc comment

--- Skyline.java
+++ Skyline.java
@@ -1,23 +1,22 @@
-//********************************************************************
-//  xxx.java       Author: Lewis/Loftus
-//
-//  Demonstrates 
-//********************************************************************
+/**
+ * Skyline --- program to display a pretty skyline
+ * @author     Lewis/Loftus
+ * @version    1.0
+ * @since      2016-09-22
+*/
 
 import javax.swing.*;
 import java.awt.*;
 
 public class Skyline
 {
-   //-----------------------------------------------------------------
-   //  Presents a collection of circles.
-   //-----------------------------------------------------------------
+   //displays the skyline in a JFrame
    public static void main (String[] args)
    {
-      JFrame frame = new JFrame ("Splat");
+      JFrame frame = new JFrame ("Skyline");
       frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 
-      frame.getContentPane().add(new SplatPanel());
+      frame.getContentPane().add(new SkylinePanel());
 
       frame.pack();
       frame.setVisible(true);
