3e15a42da661550a22cdb49958a8c0a92e3dc207
DavidYeLuo <dayeluo@galileoweb.org>
2016-09-22 19:36:27 -0700

    Changed SplatPanel to SkylinePanel
    Changed the comment to JavaDoc in the top. 
    Deleted circle object that should be replaced with buildings

--- SplatPanel.java
+++ SplatPanel.java
@@ -1,27 +1,20 @@
-//********************************************************************
-//  SplatPanel.java       Author: Lewis/Loftus
-//
-//  Demonstrates the use of graphical objects.
-//********************************************************************
-
+/**
+ * SkylinePanle.java
+ * 
+ * Demonstrate the use of graphical objects.
+ */
 import javax.swing.*;
 import java.awt.*;
 
-public class SplatPanel extends JPanel
+public class SkylinePanel extends JPanel
 {
    private Circle circle1, circle2, circle3, circle4, circle5;
 
    //-----------------------------------------------------------------
    //  Constructor: Creates five Circle objects.
    //-----------------------------------------------------------------
-   public SplatPanel()
+   public SkylinePanel()
    {
-      circle1 = new Circle (30, Color.red, 70, 35);
-      circle2 = new Circle (50, Color.green, 30, 20);
-      circle3 = new Circle (100, Color.cyan, 60, 85);
-      circle4 = new Circle (45, Color.yellow, 170, 30);
-      circle5 = new Circle (60, Color.blue, 200, 60);
-
       setPreferredSize (new Dimension(300, 200));
       setBackground (Color.black);
    }
@@ -32,11 +25,5 @@ public class SplatPanel extends JPanel
    public void paintComponent (Graphics page)
    {
       super.paintComponent(page);
-
-      circle1.draw(page);
-      circle2.draw(page);
-      circle3.draw(page);
-      circle4.draw(page);
-      circle5.draw(page);
    }
 }
