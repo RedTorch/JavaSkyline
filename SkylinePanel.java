3869152dc158d953f061615e01bec43dfa778890
Kenta Medina <kenta.medina@gmail.com>
2016-09-22 19:52:12 -0700

    Update and rename SplatPanel.java to SkylinePanel.java

---
+++ SkylinePanel.java
@@ -0,0 +1,24 @@
+/**
+ * SkylinePanel --- program to 
+ * @author          Lewis/Loftus, modified by David
+ * @version         1.0
+ * @since           2016-09-22
+*/
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SkylinePanel extends JPanel
+{
+
+   public SkylinePanel()
+   {
+      setPreferredSize (new Dimension(300, 200));
+      setBackground (Color.black);
+   }
+
+   public void paintComponent (Graphics page)
+   {
+      super.paintComponent(page);
+   }
+}
