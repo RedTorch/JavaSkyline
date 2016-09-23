d771d7352f32d2ffc8df3f0e7dece5d537ce541a
Kenta Medina <kenta.medina@gmail.com>
2016-09-22 19:31:57 -0700

    renamed Splat.java to Skyline.java

--- Skyline.java
+++ Skyline.java
@@ -7,7 +7,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Splat
+public class Skyline
 {
    //-----------------------------------------------------------------
    //  Presents a collection of circles.
