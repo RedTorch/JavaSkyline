f20fcb1d59671d03c4b463ecfda9254cb08001a6
Kenta Medina <kenta.medina@gmail.com>
2016-09-22 20:39:24 -0700

    some stuff? (edits by David/TC)

--- SkylinePanel.java
+++ SkylinePanel.java
@@ -1,6 +1,6 @@
 /**
- * SkylinePanel --- program to 
- * @author          Lewis/Loftus, modified by David
+ * SkylinePanel --- program to draw buildings
+ * @author          Lewis/Loftus, modified by David Ye Luo
  * @version         1.0
  * @since           2016-09-22
 */
@@ -10,15 +10,31 @@ import java.awt.*;
 
 public class SkylinePanel extends JPanel
 {
+   private Building building1, building2, building3, building4, building5;
 
+   //-------------------------------------------------
+   //   Default Constructor: Sets up the Panel
+   //-------------------------------------------------
    public SkylinePanel()
-   {
-      setPreferredSize (new Dimension(300, 200));
-      setBackground (Color.black);
-   }
+   {  
+       building1= new Building ();
+       building2= new Building ();
+       building3= new Building ();
+       building4= new Building ();
+       building5= new Building ();
 
+      setPreferredSize (new Dimension(300, 200));
+      setBackground (Color.cyan);
+   }
+   //-------------------------------------------------
+   //   Draws the Panel
+   //-------------------------------------------------
    public void paintComponent (Graphics page)
    {
       super.paintComponent(page);
+  
+      
+      
+      
    }
 }
